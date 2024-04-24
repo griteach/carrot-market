55bbc86aa8ff0de0a4fe2173e8941999ff9ae228
griteach <griteach22@gmail.com>
2024-04-24 12:25:33 +0900

    products load item

--- app/(tabs)/products/page.tsx
+++ app/(tabs)/products/page.tsx
@@ -1,8 +1,26 @@
+import ListProducts from "@/components/list-products";
+import db from "@/lib/db";
+
 async function getProducts() {
-  await new Promise((resolve) => setTimeout(resolve, 10000));
+  const products = await db.product.findMany({
+    select: {
+      title: true,
+      price: true,
+      created_at: true,
+      photo: true,
+      id: true,
+    },
+  });
+  return products;
 }
 
 export default async function Products() {
   const products = await getProducts();
-  return <h1>Products</h1>;
+  return (
+    <div className="p-5 flex flex-col gap-5">
+      {products.map((product) => (
+        <ListProducts key={product.id} {...product} />
+      ))}
+    </div>
+  );
 }
