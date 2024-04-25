fccb434e83c92eca98ee20ab3458208d05acfc5e
griteach <griteach22@gmail.com>
2024-04-25 12:17:59 +0900

    Product upload

---
+++ app/(tabs)/products/add/actions.ts
@@ -0,0 +1,69 @@
+"use server";
+
+import { z } from "zod";
+import fs from "fs/promises";
+import db from "@/lib/db";
+import getSession from "@/lib/session";
+import { redirect } from "next/navigation";
+
+const productSchema = z.object({
+  photo: z.string({
+    required_error: "photo is requried.",
+  }),
+  title: z
+    .string({
+      required_error: "title is requried.",
+    })
+    .max(20),
+  description: z
+    .string({
+      required_error: "description is requried.",
+    })
+    .min(5)
+    .max(50),
+  price: z.coerce.number({
+    required_error: "price is requried.",
+  }),
+});
+
+export async function uploadProduct(_: any, formData: FormData) {
+  const data = {
+    photo: formData.get("photo"),
+    title: formData.get("title"),
+    price: formData.get("price"),
+    description: formData.get("description"),
+  };
+  if (data.photo instanceof File) {
+    const photoData = await data.photo.arrayBuffer();
+    await fs.appendFile(`./public/${data.photo.name}`, Buffer.from(photoData));
+    data.photo = `/${data.photo.name}`;
+  }
+
+  const result = productSchema.safeParse(data);
+  if (!result.success) {
+    return result.error.flatten();
+  } else {
+    const session = await getSession();
+    if (session.id) {
+      const product = await db.product.create({
+        data: {
+          title: result.data.title,
+          description: result.data.description,
+          price: result.data.price,
+          photo: result.data.photo,
+          user: {
+            connect: {
+              id: session.id,
+            },
+          },
+        },
+        select: {
+          id: true,
+        },
+      });
+      redirect(`/products/${product.id}`);
+    }
+  }
+
+  //   console.log(data);
+}
--- components/product-list.tsx
+++ components/product-list.tsx
@@ -14,15 +14,23 @@ export default function ProductList({ initialProducts }: ProductListProps) {
   const [isLoading, setIsLoading] = useState(false);
   const [isLastPage, setIsLastPage] = useState(false);
   const [page, setPage] = useState(0);
+
+  //추적하기 위한 트리거 생성
+  //useRef를 사용하면 어떤 엘리멘트를 참조할지 정해줄 수 있음
+  //getElementById를 해서 해당 element를 찾아오는 것과 같은 의미
   const trigger = useRef<HTMLSpanElement>(null);
   useEffect(() => {
+    //옵저버를 생성하기 intersectionObserver
+    //추적하고자 하는 엘리멘트가 보이면 뭔가를 실행
     const observer = new IntersectionObserver(
       async (
-        entries: IntersectionObserverEntry[],
+        entries: IntersectionObserverEntry[], //여러개의 엘리멘트를 추적할수도 있으니까 기본적으로 배열 형태
         observer: IntersectionObserver
       ) => {
-        const element = entries[0];
+        const element = entries[0]; //그 중 첫번째 녀석 가져오기
         if (element.isIntersecting && trigger.current) {
+          //추적하는 element가 isIntersecting되었다면 일단 관찰을 중단한다.
+          //사용자에게 보여지고 있으니까 추적하지 않는다.
           observer.unobserve(trigger.current);
           setIsLoading(true);
           const newProducts = await getMoreProducts(page + 1);
@@ -40,6 +48,10 @@ export default function ProductList({ initialProducts }: ProductListProps) {
     if (trigger.current) {
       observer.observe(trigger.current);
     }
+
+    //이 페이지가 종료될 때 observer를 완전히 kill 해줘야 한다.
+    //사라진 컴포넌트를 계속 추적하게 하지 말아야 한다.
+    //자원을 관리해야 한다는 의미.
     return () => {
       observer.disconnect();
     };
@@ -55,8 +67,7 @@ export default function ProductList({ initialProducts }: ProductListProps) {
       {!isLastPage ? (
         <span
           ref={trigger}
-          style={{ marginTop: `${page + 1 * 900}vh` }}
-          className="mb-96 text-sm font-semibold bg-orange-500 w-fit mx-auto px-3 py-2 rounded-md hover:opacity-90 active:scale-95"
+          className="text-sm font-semibold bg-orange-500 w-fit mx-auto px-3 py-2 rounded-md hover:opacity-90 active:scale-95"
         >
           {isLoading ? "Loading..." : "Load more"}
         </span>
