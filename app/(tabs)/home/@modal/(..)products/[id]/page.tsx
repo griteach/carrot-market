519aa73841d5906a88a8ed3bf8a2a46aa06185f7
griteach <griteach22@gmail.com>
2024-04-28 10:03:51 +0900

    Modal fin

--- app/(tabs)/home/@modal/(..)products/[id]/page.tsx
+++ app/(tabs)/home/@modal/(..)products/[id]/page.tsx
@@ -1,3 +1,16 @@
-export default function Modal() {
-  return <h1>Im Modal!</h1>;
+import { PhotoIcon, XMarkIcon } from "@heroicons/react/24/solid";
+
+export default async function Modal({ params }: { params: { id: string } }) {
+  return (
+    <div className="absolute w-full h-full z-50 flex justify-center items-center bg-black bg-opacity-60 left-0 top-0">
+      <button className="absolute right-20 top-20 text-neutral-200">
+        <XMarkIcon className="size-10" />
+      </button>
+      <div className="max-w-screen-sm h-1/2 flex justify-center w-full">
+        <div className="aspect-square bg-neutral-700 rounded-md text-neutral-200 flex justify-center items-center">
+          <PhotoIcon className="h-28" />
+        </div>
+      </div>
+    </div>
+  );
 }
