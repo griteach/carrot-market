4391e6c99a44a9d005b217ae8a05c72f8f4873a3
griteach <griteach22@gmail.com>
2024-05-02 10:26:32 +0900

    ...

--- components/like-button.tsx
+++ components/like-button.tsx
@@ -18,7 +18,13 @@ export default function LikeButton({
 }: LikeButtonProps) {
   //첫 번째 인자는 mutation이 발생하기 전에 유저에게 보여주어야 할 데이터
   const [state, reducerFn] = useOptimistic(
-    { isLiked, likeCount },
+    { isLiked, likeCount }, //initial state 초기값
+
+    //return 값 변경될 것으로 예상되는 데이터를 미리 보여주기 위해 만들어준다.
+    //실제 데이터는 db나 어디선가 어떻게 됐겠지 뭐.. 라고 생각하는 방식
+    //실제 데이터가 완료될때까지 기다리면 너무 오래걸리니까. 그냥 됐겠지 하고 기대되는 결과를
+    //먼저 보여주는 것
+    //payload는 ReducerFn에서 무언가를 받아와서 이용할때 사용한다.
     (previousState, payload) => ({
       isLiked: !previousState.isLiked,
       likeCount: previousState.isLiked
